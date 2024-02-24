36eb301db873e1a380abcca4fa39f24075a3274d
iaucosolutions <hirajasekaran@gmail.com>
2024-02-24 13:03:02 +0530

    New Controller method

---
+++ Controllers/SampleController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureDemoProj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            string outputStr = "Welcome this page";
+            return outputStr;
+        }
+    }
+}
